ed88ee54aa6b93ddab6f8cc02953f1a2d3e1b150
Hugo Gao <Hugo-Gao@users.noreply.github.com>
2018-11-15 01:02:08 +0800

    Update install-on-my-zsh.sh

--- install-on-my-zsh.sh
+++ install-on-my-zsh.sh
@@ -6,5 +6,6 @@ ZSH_CUSTOM=~/.oh-my-zsh/custom
 sudo sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-wget https://raw.githubusercontent.com/Hugo-Gao/Linux-tools/6ba0bdd87a81992c23328583ba4f2f1ab9c26e69/.zshrc
+rm ~/.zshrc
+wget https://raw.githubusercontent.com/Hugo-Gao/Linux-tools/e7af6fa6f3b9c308a21831fb641022a635688576/.zshrc
 sudo chsh -s /bin/zsh
